ac82178c71c95be41428505d794dc012d7a3bc07
Nick Plucker <pluck011@morris.umn.edu>
2018-08-30 11:10:38 -0500

    Add script to compile the c program

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+num=$1
+dir_path=$2
+
+#echo The value of num is $num
+#echo The path is $dir_path
+
+tar -xzf NthPrime.tgz -C $dir_path
+
+cd $dir_path/NthPrime
+
+gcc -o NthPrime main.c nth_prime.c nth_prime.h
+
+./NthPrime $num
