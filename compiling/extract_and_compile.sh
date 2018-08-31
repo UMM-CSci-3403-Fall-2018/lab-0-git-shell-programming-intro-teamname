a404e02bd8128b732529af27147885bd10d6fc4a
Nick Plucker <pluck011@umn.edu>
2018-08-30 23:17:17 -0500

    Add comments and improve readability of script

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+# Setting the given parameters to variables 
 num=$1
 dir_path=$2
 
-#echo The value of num is $num
-#echo The path is $dir_path
-
+# Unzip the tar file and put it in the given directory path 
 tar -xzf NthPrime.tgz -C $dir_path
 
 cd $dir_path/NthPrime
 
-gcc -o NthPrime main.c nth_prime.c nth_prime.h
+# Compile the C code into an executable 
+gcc -o NthPrime main.c nth_prime.c
 
+# Run the C executable with the given number 
 ./NthPrime $num
